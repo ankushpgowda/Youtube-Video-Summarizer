20554c2dca9c66d8167264cbc0c2d049a749447b
ANKUSH P GOWDA <ankushpgowda@Ankushs-MacBook-Air-2.local>
2024-03-16 02:13:07 +0000

    making it better

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template, request
 from app import summarize
+import re
 app = Flask(__name__)
+
+def get_video_id(link):
+    match = re.search(r"(?<=v=|v/|vi=|youtu\.be/|embed/)[^#\?]*", link)
+    return match.group(0) if match else None
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
     summary = ""
     if request.method == 'POST':
         link = request.form.get('link')
-        pro_link = link.split('=')
-        summary = summarize(pro_link[-1])
+        pro_link = get_video_id(link)
+        summary = summarize(pro_link)
     return render_template("index.html", summary=summary)
 
 if __name__ == "__main__":
